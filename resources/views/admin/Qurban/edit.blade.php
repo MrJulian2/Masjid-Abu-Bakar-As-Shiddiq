09aa29999477b4a617a5c88583715c0d661f909a
Naufal Farros <naufalfarros05@gmail.com>
2026-05-21 20:31:28 +0700

    Edit Data Qurban

--- resources/views/admin/Qurban/edit.blade.php
+++ resources/views/admin/Qurban/edit.blade.php
@@ -1,318 +1,194 @@
 @extends('admin.index')
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
+    <section class="content">
+        <div class="container-fluid">
 
-        <div class="card">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Edit Data Penerima Qurban</h3>
+                </div>
 
-            <div class="card-header">
-                <h3 class="card-title">
-                    Edit Data Penerima Qurban
-                </h3>
-            </div>
+                <div class="card-body">
 
-            <div class="card-body">
+                    <form action="{{ route('qurban.update', $qurban->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
 
-                <form action="{{ route('qurban.update', $qurban->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" name="nama" class="form-control"
+                                value="{{ $qurban->nama }}">
+                        </div>
 
-                    {{-- NAMA --}}
-                    <div class="form-group">
-                        <label>Nama</label>
-                        <input type="text"
-                               name="nama"
-                               class="form-control"
-                               value="{{ $qurban->nama }}">
-                    </div>
+                        <div class="form-group">
+                            <label>No HP</label>
+                            <input type="text" name="nomor_hp" class="form-control"
+                                value="{{ $qurban->nomor_hp }}">
+                        </div>
 
-                    {{-- NO HP --}}
-                    <div class="form-group">
-                        <label>No HP</label>
-                        <input type="text"
-                               name="nomor_hp"
-                               class="form-control"
-                               value="{{ $qurban->nomor_hp }}">
-                    </div>
+                        <div class="form-group">
+                            <label>Alamat</label>
+                            <textarea name="alamat" class="form-control">{{ $qurban->alamat }}</textarea>
+                        </div>
 
-                    {{-- ALAMAT --}}
-                    <div class="form-group">
-                        <label>Alamat</label>
-                        <textarea name="alamat"
-                                  rows="3"
-                                  class="form-control">{{ $qurban->alamat }}</textarea>
-                    </div>
+                        <div class="form-group">
+                            <label>RT</label>
+                            <input type="text" name="rt" class="form-control"
+                                value="{{ $qurban->rt }}">
+                        </div>
 
-                    {{-- RT --}}
-                    <div class="form-group">
-                        <label>RT</label>
-                        <input type="text"
-                               name="rt"
-                               class="form-control"
-                               value="{{ $qurban->rt }}">
-                    </div>
+                        <div class="form-group">
+                            <label>RW</label>
+                            <input type="text" name="rw" class="form-control"
+                                value="{{ $qurban->rw }}">
+                        </div>
 
-                    {{-- RW --}}
-                    <div class="form-group">
-                        <label>RW</label>
-                        <input type="text"
-                               name="rw"
-                               class="form-control"
-                               value="{{ $qurban->rw }}">
-                    </div>
+                        <div class="form-group">
+                            <label>Jumlah Kupon</label>
+                            <input type="number" name="jumlah_kupon" class="form-control"
+                                value="{{ count($kupon) }}" min="1">
+                        </div>
 
-                    {{-- JUMLAH KUPON --}}
-                    <div class="form-group">
-                        <label>Jumlah Kupon</label>
-                        <input type="number"
-                               name="jumlah_kupon"
-                               class="form-control"
-                               value="{{ count($kupon) }}"
-                               min="1">
-                    </div>
+                        <hr>
 
-                    <hr>
+                        <h5>Daftar QR Code</h5>
 
-                    {{-- TABLE --}}
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h5 class="mb-0">Daftar QR Code</h5>
-                    </div>
+                        {{-- WRAPPER RESPONSIVE --}}
+                        <div class="table-responsive">
 
-                    <div class="table-responsive">
+                            <table id="qr-table"
+                                class="table table-bordered table-striped nowrap"
+                                width="100%">
 
-                        <table id="qr-table"
-                               class="table table-bordered table-striped table-hover nowrap"
-                               width="100%">
-
-                            <thead class="thead-dark">
-                                <tr>
-                                    <th width="40">No</th>
-                                    <th>Kode QR</th>
-                                    <th width="120">QR Code</th>
-                                    <th width="140">Status</th>
-                                    <th>DiScan Oleh</th>
-                                    <th width="170">DiScan Pada</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-
-                                @foreach ($kupon as $key => $k)
+                                <thead>
                                     <tr>
-
-                                        {{-- NO --}}
-                                        <td class="text-center">
-                                            {{ $key + 1 }}
-                                        </td>
-
-                                        {{-- KODE QR --}}
-                                        <td>
-                                            <span class="font-weight-bold">
-                                                {{ $k->qr_code }}
-                                            </span>
-                                        </td>
-
-                                        {{-- QR --}}
-                                        <td class="text-center">
-
-                                            <div class="qr-wrapper">
-                                                {!! QrCode::size(70)->generate($k->qr_code) !!}
-                                            </div>
-
-                                        </td>
-
-                                        {{-- STATUS --}}
-                                        <td class="text-center">
-
-                                            @if ($k->status == 'belum_diambil')
-
-                                                <span class="badge badge-danger px-3 py-2">
-                                                    Belum Diambil
-                                                </span>
-
-                                            @elseif ($k->status == 'sudah_diambil')
-
-                                                <span class="badge badge-success px-3 py-2">
-                                                    Sudah Diambil
-                                                </span>
-
-                                            @endif
-
-                                        </td>
-
-                                        {{-- SCANNED BY --}}
-                                        <td>
-
-                                            @if ($k->scanned_by)
-                                                {{ $k->scannedBy->name }}
-                                            @else
-                                                -
-                                            @endif
-
-                                        </td>
-
-                                        {{-- SCANNED AT --}}
-                                        <td>
-
-                                            @if ($k->scanned_at)
-
-                                                {{ \Carbon\Carbon::parse($k->scanned_at)->format('d M Y H:i') }}
-
-                                            @else
-                                                -
-                                            @endif
-
-                                        </td>
-
+                                        <th>No</th>
+                                        <th>Kode QR</th>
+                                        <th>QR Code</th>
+                                        <th>Status</th>
+                                        <th>DiScan Oleh</th>
+                                        <th>DiScan Pada</th>
                                     </tr>
-                                @endforeach
+                                </thead>
 
-                            </tbody>
+                                <tbody>
+                                    @foreach ($kupon as $key => $k)
+                                        <tr>
+                                            <td>{{ $key + 1 }}</td>
 
-                        </table>
+                                            <td>{{ $k->qr_code }}</td>
 
-                    </div>
+                                            <td style="width: 100px;">
+                                                {!! QrCode::size(80)->generate($k->qr_code) !!}
+                                            </td>
 
-                    {{-- BUTTON --}}
-                    <div class="mt-3">
+                                            <td>
+                                                @if ($k->status == 'belum_diambil')
+                                                    <span class="badge badge-danger">
+                                                        Belum Diambil
+                                                    </span>
+                                                @elseif ($k->status == 'sudah_diambil')
+                                                    <span class="badge badge-success">
+                                                        Sudah Diambil
+                                                    </span>
+                                                @endif
+                                            </td>
 
-                        <button class="btn btn-primary" type="submit">
-                            <i class="fas fa-save"></i>
+                                            <td>
+                                                @if ($k->scanned_by)
+                                                    {{ $k->scannedBy->name }}
+                                                @else
+                                                    -
+                                                @endif
+                                            </td>
+
+                                            <td>
+                                                @if ($k->scanned_at)
+                                                    {{ \Carbon\Carbon::parse($k->scanned_at)->format('d M Y H:i') }}
+                                                @else
+                                                    -
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+
+                        </div>
+
+                        <button class="btn btn-primary mt-3" type="submit">
                             Update
                         </button>
 
-                        <a href="{{ route('qurban.index') }}"
-                           class="btn btn-secondary">
-                            Kembali
-                        </a>
+                    </form>
 
-                    </div>
-
-                </form>
-
+                </div>
             </div>
+
         </div>
-
-    </div>
-</section>
+    </section>
 @endsection
-
 
 @section('script')
 
-{{-- SWEET ALERT --}}
-<script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    {{-- SWEET ALERT --}}
+    <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
 
-{{-- DATATABLES --}}
-<script src="{{ asset('AdminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    {{-- MOMENT --}}
+    <script src="{{ asset('AdminLTE/plugins/moment/moment.min.js') }}"></script>
 
-<style>
+    {{-- DATE RANGE PICKER --}}
+    <script src="{{ asset('AdminLTE/plugins/daterangepicker/daterangepicker.js') }}"></script>
 
-/* =========================
-   TABLE FIX
-========================= */
-#qr-table {
-    width: 100% !important;
-}
+    {{-- BOOTSTRAP --}}
+    <script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-/* =========================
-   TABLE HEAD
-========================= */
-#qr-table thead th {
-    text-align: center;
-    vertical-align: middle !important;
-    white-space: nowrap;
-    font-size: 13px;
-}
+    {{-- TEMPUSDOMINUS --}}
+    <script src="{{ asset('AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 
-/* =========================
-   TABLE BODY
-========================= */
-#qr-table tbody td {
-    vertical-align: middle !important;
-    font-size: 13px;
-}
+    {{-- DATATABLE --}}
+    <script src="{{ asset('AdminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
 
-/* =========================
-   QR FIX
-========================= */
-.qr-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+    <script src="{{ asset('AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
-.qr-wrapper svg {
-    width: 70px !important;
-    height: 70px !important;
-}
+    <script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
 
-/* =========================
-   DATATABLE RESPONSIVE
-========================= */
-.dataTables_wrapper {
-    width: 100%;
-}
+    <script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 
-.dataTables_scrollBody {
-    overflow-x: auto !important;
-}
+    <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
 
-/* =========================
-   MOBILE
-========================= */
-@media screen and (max-width: 768px) {
+    <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
 
-    #qr-table thead th,
-    #qr-table tbody td {
-        font-size: 12px;
-    }
+    <script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
 
-    .qr-wrapper svg {
-        width: 55px !important;
-        height: 55px !important;
-    }
-}
-
-</style>
-
-<script>
-
-$(document).ready(function () {
-
-    $('#qr-table').DataTable({
-
-        responsive: true,
-        autoWidth: false,
-        scrollX: true,
-
-        paging: true,
-        lengthChange: true,
-        searching: true,
-        ordering: true,
-        info: true,
-
-        language: {
-            search: "Cari:",
-            lengthMenu: "Tampilkan _MENU_ data",
-            zeroRecords: "Data tidak ditemukan",
-            info: "Menampilkan _START_ - _END_ dari _TOTAL_ data",
-            infoEmpty: "Tidak ada data",
-            paginate: {
-                first: "Awal",
-                last: "Akhir",
-                next: "→",
-                previous: "←"
-            }
+    <style>
+        #qr-table td,
+        #qr-table th {
+            vertical-align: middle;
+            white-space: nowrap;
         }
 
-    });
+        #qr-table svg {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
 
-});
+    <script>
+        $(function() {
 
-</script>
+            $('#qr-table').DataTable({
+                responsive: true,
+                autoWidth: false,
+                lengthChange: true,
+                searching: true,
+                ordering: true,
+                paging: true,
+                info: true,
+            });
+
+        });
+    </script>
 @endsection
