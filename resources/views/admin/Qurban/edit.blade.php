45819c91b5e88263975a94cdae7c7ddf432ef045
Naufal Farros <naufalfarros05@gmail.com>
2026-05-21 20:33:15 +0700

    Edit Data Qurban

--- resources/views/admin/Qurban/edit.blade.php
+++ resources/views/admin/Qurban/edit.blade.php
@@ -17,14 +17,12 @@
 
                         <div class="form-group">
                             <label>Nama</label>
-                            <input type="text" name="nama" class="form-control"
-                                value="{{ $qurban->nama }}">
+                            <input type="text" name="nama" class="form-control" value="{{ $qurban->nama }}">
                         </div>
 
                         <div class="form-group">
                             <label>No HP</label>
-                            <input type="text" name="nomor_hp" class="form-control"
-                                value="{{ $qurban->nomor_hp }}">
+                            <input type="text" name="nomor_hp" class="form-control" value="{{ $qurban->nomor_hp }}">
                         </div>
 
                         <div class="form-group">
@@ -34,95 +32,91 @@
 
                         <div class="form-group">
                             <label>RT</label>
-                            <input type="text" name="rt" class="form-control"
-                                value="{{ $qurban->rt }}">
+                            <input type="text" name="rt" class="form-control" value="{{ $qurban->rt }}">
                         </div>
 
                         <div class="form-group">
                             <label>RW</label>
-                            <input type="text" name="rw" class="form-control"
-                                value="{{ $qurban->rw }}">
+                            <input type="text" name="rw" class="form-control" value="{{ $qurban->rw }}">
                         </div>
 
                         <div class="form-group">
                             <label>Jumlah Kupon</label>
-                            <input type="number" name="jumlah_kupon" class="form-control"
-                                value="{{ count($kupon) }}" min="1">
+                            <input type="number" name="jumlah_kupon" class="form-control" value="{{ count($kupon) }}"
+                                min="1">
                         </div>
 
                         <hr>
-
-                        <h5>Daftar QR Code</h5>
-
-                        {{-- WRAPPER RESPONSIVE --}}
-                        <div class="table-responsive">
-
-                            <table id="qr-table"
-                                class="table table-bordered table-striped nowrap"
-                                width="100%">
-
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Kode QR</th>
-                                        <th>QR Code</th>
-                                        <th>Status</th>
-                                        <th>DiScan Oleh</th>
-                                        <th>DiScan Pada</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    @foreach ($kupon as $key => $k)
-                                        <tr>
-                                            <td>{{ $key + 1 }}</td>
-
-                                            <td>{{ $k->qr_code }}</td>
-
-                                            <td style="width: 100px;">
-                                                {!! QrCode::size(80)->generate($k->qr_code) !!}
-                                            </td>
-
-                                            <td>
-                                                @if ($k->status == 'belum_diambil')
-                                                    <span class="badge badge-danger">
-                                                        Belum Diambil
-                                                    </span>
-                                                @elseif ($k->status == 'sudah_diambil')
-                                                    <span class="badge badge-success">
-                                                        Sudah Diambil
-                                                    </span>
-                                                @endif
-                                            </td>
-
-                                            <td>
-                                                @if ($k->scanned_by)
-                                                    {{ $k->scannedBy->name }}
-                                                @else
-                                                    -
-                                                @endif
-                                            </td>
-
-                                            <td>
-                                                @if ($k->scanned_at)
-                                                    {{ \Carbon\Carbon::parse($k->scanned_at)->format('d M Y H:i') }}
-                                                @else
-                                                    -
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-
-                            </table>
-
-                        </div>
-
                         <button class="btn btn-primary mt-3" type="submit">
                             Update
                         </button>
 
                     </form>
+                    <h5>Daftar QR Code</h5>
+
+                    {{-- WRAPPER RESPONSIVE --}}
+                    <div class="table-responsive">
+
+                        <table id="qr-table" class="table table-bordered table-striped nowrap" width="100%">
+
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Kode QR</th>
+                                    <th>QR Code</th>
+                                    <th>Status</th>
+                                    <th>DiScan Oleh</th>
+                                    <th>DiScan Pada</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($kupon as $key => $k)
+                                    <tr>
+                                        <td>{{ $key + 1 }}</td>
+
+                                        <td>{{ $k->qr_code }}</td>
+
+                                        <td style="width: 100px;">
+                                            {!! QrCode::size(80)->generate($k->qr_code) !!}
+                                        </td>
+
+                                        <td>
+                                            @if ($k->status == 'belum_diambil')
+                                                <span class="badge badge-danger">
+                                                    Belum Diambil
+                                                </span>
+                                            @elseif ($k->status == 'sudah_diambil')
+                                                <span class="badge badge-success">
+                                                    Sudah Diambil
+                                                </span>
+                                            @endif
+                                        </td>
+
+                                        <td>
+                                            @if ($k->scanned_by)
+                                                {{ $k->scannedBy->name }}
+                                            @else
+                                                -
+                                            @endif
+                                        </td>
+
+                                        <td>
+                                            @if ($k->scanned_at)
+                                                {{ \Carbon\Carbon::parse($k->scanned_at)->format('d M Y H:i') }}
+                                            @else
+                                                -
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
+
 
                 </div>
             </div>
@@ -132,7 +126,6 @@
 @endsection
 
 @section('script')
-
     {{-- SWEET ALERT --}}
     <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
 
